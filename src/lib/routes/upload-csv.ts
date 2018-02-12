fb3fcc98593f1c947a719ee720b991e261e9830e
Denis Tokarev <github@devlato.com>
2018-02-12 14:27:03 +0100

    Fix issue

--- src/lib/routes/upload-csv.ts
+++ src/lib/routes/upload-csv.ts
@@ -1,20 +1,23 @@
 import { Context } from 'koa';
-import { Configuration } from 'csv-wealth-api';
 import { convertCSV } from '../utils';
 import { getClient } from '../db';
 
-export default (config: Configuration) => async (ctx: Context) => {
+export default () => async (ctx: Context) => {
   const records = await convertCSV(ctx.request.body);
   const client = await getClient();
 
   const collection = client.collection('records');
-  collection.insertMany(records, (error, result) => {
-    if (!error) {
-      console.log('Saved successfully =', result);
-      ctx.body = 'Success';
-    } else {
-      console.log('Error', error.stack);
-      ctx.body = 'Error';
-    }
-  });
+  await (() => new Promise((resolve, reject) => {
+    collection.insertMany(records, (error, result) => {
+      if (!error) {
+        console.log('Saved successfully =', result);
+        ctx.body = 'Success';
+        resolve();
+      } else {
+        console.log('Error', error.stack);
+        ctx.body = 'Error';
+        reject();
+      }
+    });
+  }))();
 };
